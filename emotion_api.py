f99cb93d0c6eb0dbae1fad9d8aaf6ad6e4ae66c3
sarah <sarahahmedal147@gmail.com>
2025-05-01 10:15:29 -0700

    Fix port binding for Render

--- emotion_api.py
+++ emotion_api.py
@@ -20,4 +20,8 @@ def analyze_emotion():
     
     return jsonify(emotion_result)
 
-app.run(host='0.0.0.0', port=5000)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
