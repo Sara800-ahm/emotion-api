a31b77927fe5efc3a30168897ffd61e158e95452
sarah <sarahahmedal147@gmail.com>
2025-05-12 09:11:43 -0700

    أضفت مسار root لحل مشكلة 404

--- test_request.py
+++ test_request.py
@@ -6,7 +6,7 @@ with open("fg.jpeg", "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
 # أرسل الصورة إلى السيرفر
-response = requests.post("http://127.0.0.1:5000/analyze", json={"image": encoded_string})
+response = requests.post("http://127.0.0.1:5000/", json={"image": encoded_string})
 
 # اطبع النتيجة
 print(response.json())
